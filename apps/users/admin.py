6cf5e465e04a6af3ed414ed2fdb60d91795de6f9
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-10-19 21:43:12 -0500

    [signup-loging] Add user admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,46 @@
-from django.contrib import admin
+"""Users model admin."""
 
-# Register your models here.
+# Django
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+# Models
+from apps.users.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """User model admin."""
+
+    list_display = [
+        'pk',
+        'first_name', 'last_name',
+        'username', 'email',
+        'phone_number', 'role',
+        'verified', 'created', 'updated'
+    ]
+
+    list_display_links = ['pk', 'username']
+
+    readonly_fields = [
+        'pk',
+        'first_name', 'last_name',
+        'username', 'email',
+        'phone_number', 'role',
+        'created', 'updated'
+    ]
+
+    search_fields = [
+        'username', 'email',
+        'first_name', 'last_name',
+        'verified'
+    ]
+
+    list_filter = ['verified', 'role']
+    ordering = ['first_name', 'last_name']
+
+    def has_add_permission(self, request, obj=None) -> bool:
+        return False
+
+    def has_delete_permission(self, request, obj=None) -> bool:
+        return False
