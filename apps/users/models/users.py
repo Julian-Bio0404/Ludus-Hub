7d0160ec83257363067f3ec96d31c800d1f09feb
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-10-26 09:58:09 -0500

    [profile] Add profile model

--- apps/users/models/users.py
+++ apps/users/models/users.py
@@ -11,6 +11,9 @@ from djchoices import ChoiceItem, DjangoChoices
 # Models
 from apps.utils.models import SportfyModel
 
+# Utils
+from apps.utils.files import user_directory_path
+
 
 class User(SportfyModel, AbstractUser):
     """
@@ -51,3 +54,43 @@ class User(SportfyModel, AbstractUser):
     def get_short_name(self):
         """Return username."""
         return self.username
+
+
+class Profile(SportfyModel):
+    """
+    Profile model.
+    Model One to one with User model.
+    """
+
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+
+    photo = models.ImageField(
+        help_text='profile photo',
+        upload_to=user_directory_path, blank=True, null=True)
+
+    cover_photo = models.ImageField(
+        help_text='profile cover photo',
+        upload_to=user_directory_path, blank=True, null=True)
+
+    about = models.TextField(
+        help_text='write something about you', blank=True)
+
+    birth_date = models.DateField(
+        auto_now=False, auto_now_add=False, blank=True, null=True)
+
+    sport = models.CharField(
+        help_text='What sport do you play?', max_length=25, blank=True)
+
+    country = models.CharField(
+        help_text='your country of origin', max_length=60, blank=True)
+
+    public = models.BooleanField(help_text='Profile privacy', default=True)
+
+    web_site = models.URLField(
+        help_text='personal web site', max_length=200, blank=True)
+
+    social_link = models.URLField(
+        help_text='social media', max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.username
---
+++ apps/utils/files.py
@@ -0,0 +1,5 @@
+"""File utils."""
+
+
+def user_directory_path(instance, filename):
+    return f'user_{instance.user.id}/{filename}'
