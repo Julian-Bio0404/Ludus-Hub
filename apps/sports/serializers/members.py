3cd8d7d0b3eec7e844b3272fa340c82748f0a3c2
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-12-11 13:36:49 -0500

    [clubs] Add Invitation serializers

--- apps/sports/serializers/members.py
+++ apps/sports/serializers/members.py
@@ -4,10 +4,12 @@
 from rest_framework import serializers
 
 # Models
-from apps.sports.models import Member
+from apps.sports.models import Member, Invitation
+from apps.users.models import User
 
 # Serializers
 from apps.users.serializers import UserModelSerializer
+from .clubs import ClubModelSerializer
 
 
 class MemberModelSerializer(serializers.ModelSerializer):
@@ -24,3 +26,60 @@ class MemberModelSerializer(serializers.ModelSerializer):
         ]
 
         read_only_fields = ['user', 'joined_at']
+
+
+class InvitationModelSerializer(serializers.ModelSerializer):
+    """Invitation model serializer."""
+
+    sent_by = UserModelSerializer(read_only=True)
+    invited = UserModelSerializer(read_only=True)
+    club = ClubModelSerializer(read_only=True)
+    used = serializers.BooleanField()
+
+    class Meta:
+        model = Invitation
+        fields = [
+            'id', 'sent_by', 'invited',
+            'club', 'used', 'created'
+        ]
+
+        read_only_fields = [
+            'sent_by', 'invited',
+            'club', 'created'
+        ]
+
+
+class CreateInvitationSerializer(serializers.Serializer):
+    """Create Invitation serializer."""
+
+    invited = serializers.CharField()
+
+    def validate(self, data):
+        """Verify that invited exists."""
+        username = data['invited']
+        try:
+            invited = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                f'The user with username {username} does not exist')
+
+        invitation = Invitation.objects.filter(
+            invited=invited, club=self.context['club'])
+        if invitation.exists():
+            raise serializers.ValidationError(
+                'This user already has a invitation for this club.')
+
+        self.context['invited'] = invited
+        return data
+
+    def create(self, data):
+        """Create a invitation."""
+        sent_by = self.context['sent_by']
+        invited = self.context['invited']
+        club = self.context['club']
+        invitation = Invitation.objects.create(
+            sent_by=sent_by, invited=invited, club=club)
+
+        # Create a inactive Membership
+        Member.objects.create(user=invited, club=club)
+        return invitation
