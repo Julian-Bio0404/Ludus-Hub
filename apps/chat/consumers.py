f71dae351ad61c8e002137a6415dc492232e0d6a
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-12-24 01:39:04 -0500

    [chat] Apply async consumer and add authentication

--- apps/chat/consumers.py
+++ apps/chat/consumers.py
@@ -2,29 +2,28 @@
 
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     """Chat consumer."""
 
-    def connect(self):
+    async def connect(self):
         """Join room group."""
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         """Leave room group."""
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         """
         Receive message from WebSocket
         and send message to room group.
@@ -32,14 +31,14 @@ class ChatConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, {'type': 'chat_message', 'message': message}
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         """
         Receive message from room group
         and send message to WebSocket.
         """
         message = event['message']
-        self.send(text_data=json.dumps({'message': message}))
+        await self.send(text_data=json.dumps({'message': message}))
--- config/asgi.py
+++ config/asgi.py
@@ -3,12 +3,14 @@
 import os
 
 # Channels
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 # Django
 from django.core.asgi import get_asgi_application
+
+# Middelwares
+from apps.chat.middlewares import TokenAuthMiddleware
 
 # Urls
 from apps.chat.routing import websocket_urlpatterns
@@ -18,7 +20,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
+        TokenAuthMiddleware(
             URLRouter(websocket_urlpatterns)
         )
     ),
