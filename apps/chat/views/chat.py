f3d4f0cb56fdf55c674f6c5c32552ee6907db620
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-12-25 20:21:29 -0500

    [chat] Restrict rooms by members

--- apps/chat/views/chat.py
+++ apps/chat/views/chat.py
@@ -1,12 +1,26 @@
 """Chat views."""
 
 # Django
+from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+
+# Django REST Framework
+from rest_framework.authtoken.models import Token
 
 # Models
 from apps.chat.models import Room
 
 
 def room(request, room_name):
-    room = get_object_or_404(Room, slug=room_name)
-    return render(request, 'chat/room.html', {'room_name': room.slug})
+    try:
+        key = request.get_full_path().split('?token=')[1]
+        token = Token.objects.get(key=key)
+        user = token.user
+        room = get_object_or_404(Room, slug=room_name)
+        if user not in room.receivers.all():
+            raise Http404('Room does not exist.')
+        return render(request, 'chat/room.html', {'room_name': room.slug})
+    except IndexError:
+        raise Http404('Room does not exist.')
+    except Token.DoesNotExist:
+        raise Http404('Token does not exist.')
