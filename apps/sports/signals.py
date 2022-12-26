101093ed7d92ed8f0783019f5360cd04c01530aa
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-12-25 20:30:05 -0500

    [chat] Add member signal

--- apps/sports/signals.py
+++ apps/sports/signals.py
@@ -5,8 +5,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Models
-from apps.sports.models import Club
 from apps.chat.models import Room
+from apps.sports.models import Club, Member
 
 
 @receiver(post_save, sender=Club)
@@ -15,3 +15,11 @@ def club_post_save(sender, instance, created, **kwargs):
     if created:
         room = Room.objects.create(club=instance, slug=instance.slug)
         room.receivers.add(instance.trainer)
+
+
+@receiver(post_save, sender=Member)
+def member_post_save(sender, instance, created, **kwargs):
+    """Set of actions executed after a member joins the club."""
+    if created:
+        room = instance.club.room
+        room.receivers.add(instance.user)
