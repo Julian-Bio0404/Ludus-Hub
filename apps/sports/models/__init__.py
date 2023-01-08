7efa4a93f969fcd317bc92637bc658b3d034aaa4
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-01-08 01:51:43 -0500

    [assistances] Add Assistance serializers

--- apps/sports/models/__init__.py
+++ apps/sports/models/__init__.py
@@ -1,2 +1,2 @@
 from .clubs import Club  # NOQA
-from .members import Invitation, Member  # NOQA
+from .members import Invitation, Member, Assistance # NOQA
--- apps/sports/serializers/members.py
+++ apps/sports/serializers/members.py
@@ -4,7 +4,7 @@
 from rest_framework import serializers
 
 # Models
-from apps.sports.models import Member, Invitation
+from apps.sports.models import Member, Invitation, Assistance
 from apps.users.models import User
 
 # Serializers
@@ -83,3 +83,41 @@ class CreateInvitationSerializer(serializers.Serializer):
         # Create a inactive Membership
         Member.objects.create(user=invited, club=club)
         return invitation
+
+
+class AssistanceModelSerializer(serializers.ModelSerializer):
+    """Assistance model serializer."""
+
+    user = UserModelSerializer(read_only=True)
+
+    class Meta:
+        model = Assistance
+        fields = ['user', 'created']
+        read_only_fields = ['user', 'created']
+
+
+class CreateAssistanceSerializer(serializers.Serializer):
+    """Create assistance serializer."""
+
+    members = serializers.ListField(child=serializers.CharField())
+
+    def validate(self, data):
+        """Search members by club."""
+        club = self.context['club']
+        members = data['members']
+        members = club.members.filter(user__username__in=members, active=True)
+        if not members:
+            raise serializers.ValidationError(
+                'There are no existing members for this club.')
+        self.context['members'] = members
+        return data
+
+    def create(self, data):
+        """Assistances Bulk create."""
+        query = [
+            Assistance(
+                user=member,
+                club=self.context['club']
+            ) for member in self.context['members']
+        ]
+        return Assistance.objects.bulk_create(query)
