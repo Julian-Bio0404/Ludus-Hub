46e057b3e4b9a7999feda68a952192cca3582b7a
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-03-05 10:55:19 -0500

    [websockets] Add MongoDb service

--- web-2/main.py
+++ web-2/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from app.queries import clubs
+from app.schemas.clubs import ClubSchema
+from fastapi import Depends, FastAPI, HTTPException
+from settings import get_db
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -6,3 +10,11 @@ app = FastAPI()
 @app.get('/main')
 def main():
     return 'Hello World'
+
+
+@app.get('/club/{slug}', response_model=ClubSchema)
+def read_club(slug: str, db: Session = Depends(get_db)):
+    club = clubs.get_club(db, slug=slug)
+    if not club:
+        raise HTTPException(status_code=404, detail='Club does not exists')
+    return club
