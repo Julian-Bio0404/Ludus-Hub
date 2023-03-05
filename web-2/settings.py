3556da419f7589431a14ae91f1881dfbc32f14b3
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-03-05 11:14:39 -0500

    [websockets] Create connection to MongoDb

--- web-2/settings.py
+++ web-2/settings.py
@@ -1,12 +1,13 @@
 """Web 2 settings."""
 
 from functools import lru_cache
+from pathlib import Path
 
 from pydantic import BaseSettings
+from pymongo import MongoClient
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from pathlib import Path
 
 
 class Settings(BaseSettings):
@@ -17,6 +18,9 @@ class Settings(BaseSettings):
     postgres_password: str
     postgres_host: str
     postgres_port: str
+
+    mongo_initdb_root_username: str
+    mongo_initdb_root_password: str
 
     class Config:
         env_file = str(Path(__file__).parent.parent / ".env")
@@ -32,15 +36,26 @@ settings = get_settings()
 DATABASE_URL = f'postgresql://{settings.postgres_user}:{settings.postgres_password}\
 @{settings.postgres_host}/{settings.postgres_db}'
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+MONGODB_URL = f'mongodb://{settings.mongo_initdb_root_username}:\
+{settings.mongo_initdb_root_password}@mongodb:27017'
+
+postgres_engine = create_engine(DATABASE_URL)
+mongo_client = MongoClient(MONGODB_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=postgres_engine)
 
 Base = declarative_base()
 
 
 def get_db():
+    """Get session of postgres db."""
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+def get_mongodb():
+    """Get session of mongo db."""
+    return mongo_client.mydatabase
