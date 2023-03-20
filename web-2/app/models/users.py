f85c57f645eee7e5cf0feaf07a5894b24fc168dd
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-03-19 20:55:41 -0500

    [websockets] Add Token model

--- web-2/app/models/users.py
+++ web-2/app/models/users.py
@@ -1,5 +1,5 @@
 from settings import Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, DateTime, ForeignKey, String
 from sqlalchemy.orm import relationship
 
 
@@ -11,3 +11,15 @@ class User(Base):
     id = Column(String, primary_key=True, index=True)
     username = Column(String, unique=True)
     members = relationship('Member', back_populates='user')
+
+
+class Token(Base):
+    """Token model."""
+
+    __tablename__ = 'authtoken_token'
+
+    id = Column(String, primary_key=True, index=True)
+    key = Column(String, primary_key=True, unique=True)
+    user_id = Column(String, ForeignKey('users_user.id'))
+    user = relationship('User', back_populates='token')
+    created = Column(DateTime, nullable=False)
