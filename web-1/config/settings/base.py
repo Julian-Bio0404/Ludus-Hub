f80abc842e93bafa7b1ea12565b38e71f5344c37
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-03-21 21:26:43 -0500

    [websockets] Remove unnecesary libraries

--- web-1/config/settings/base.py
+++ web-1/config/settings/base.py
@@ -21,8 +21,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 DJANGO_APPS = [
-    'daphne',
-    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
