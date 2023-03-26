36d58c22514072e84013127fccc92d09cf026176
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-03-26 02:37:00 -0500

    [subscriptions] Add payments model admin

--- web-1/apps/payments/admin.py
+++ web-1/apps/payments/admin.py
@@ -1,3 +1,26 @@
+from apps.payments.models import Plan, Price
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    """Price model admin."""
+
+    list_display = ['stripe_id', 'currency', 'amount']
+    readonly_fields = ['stripe_id']
+    search_fields = ['currency']
+    list_filter = ['currency']
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    """Plan model admin."""
+
+    list_display = [
+        'stripe_id', 'name',
+        'interval', 'description',
+        'price'
+    ]
+    readonly_fields = ['stripe_id']
+    search_fields = ['name']
+    list_filter = ['interval']
