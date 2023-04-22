f3b77f882bf3c7359650704dd816538eab9bf38d
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-04-21 22:48:01 -0500

    [subscriptions] Add Create subscription serializer

--- web-1/apps/users/serializers/__init__.py
+++ web-1/apps/users/serializers/__init__.py
@@ -1,5 +1,5 @@
 from .profiles import ProfileModelSerializer  # noqa
-from .subscriptions import SubscriptionModelSerializer  # noqa
+from .subscriptions import CreateSubscriptionSerializer, SubscriptionModelSerializer  # noqa
 from .users import (AccountVerificationSerializer, RestorePasswordSerializer,  # noqa
                     TokenRestorePasswordSerializer, TokenUpdateEmailSerializers,
                     UpdateEmailSerializers, UpdatePasswordSerializer,
--- web-1/apps/users/serializers/subscriptions.py
+++ web-1/apps/users/serializers/subscriptions.py
@@ -2,6 +2,8 @@
 
 from apps.users.models import Subscription
 from rest_framework import serializers
+from apps.payments.models import Card, Plan
+from apps.utils.payments import StripeClient
 
 
 class SubscriptionModelSerializer(serializers.ModelSerializer):
@@ -16,3 +18,61 @@ class SubscriptionModelSerializer(serializers.ModelSerializer):
             'user', 'plan', 'active',
             'created', 'updated'
         ]
+
+
+class CreateSubscriptionSerializer(serializers.Serializer):
+    """Handle the subscription creation."""
+
+    plan_id = serializers.UUIDField()
+
+    payment_method = serializers.DictField()
+
+    def validate(self, data):
+        """Verify the plan id and payment mehotd."""
+        try:
+            plan = Plan.objects.get(id=data['plan_id'])
+        except Plan.DoesNotExist:
+            raise serializers.ValidationError('Plan Does not exist.')
+
+        user = self.context['user']
+        client = StripeClient()
+
+        if not user.customer_id:
+            customer = client.customer.create(**data)
+            user.customer_id = customer['id']
+            user.save()
+        else:
+            customer = client.customer.get(id=user.customer_id)
+
+        last4 = data['payment_method']['number'][-4:]
+        card = Card.objects.filter(user=user, last4=last4).last()
+        if not card:
+            response = client.customer.create_source(
+                user.customer_id, {'source': data['payment_method']['source']}
+            )
+            card = Card.objects.create(user=user, stripe_id=response['id'], last4=response['last4'])
+        self.context['plan'] = plan
+        self.context['card'] = card
+        return data
+
+    def create(self, data):
+        user = self.context['user']
+        plan = self.context['plan']
+        card = self.context['card']
+
+        subs_data = {
+            'customer': user.customer_id,
+            'items': [{'price': plan.stripe_id}],
+            'currency': plan.price.currency,
+            'default_source': card.stripe_id
+        }
+        client = StripeClient()
+        client.subscription.create(**subs_data)
+        subscription = Subscription.objects.update_or_create(
+            user=user,
+            defaults={
+                'plan': plan,
+                'active': True
+            }
+        )
+        return subscription
