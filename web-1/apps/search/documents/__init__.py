85978656c718f927135b8237085c1254096a0b9f
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-07-01 17:44:05 -0500

    [search] Add club document

---
+++ web-1/apps/search/documents/__init__.py
@@ -0,0 +1,2 @@
+from .users import UserDocument  # NOQA
+from .clubs import ClubDocument  # NOQA
---
+++ web-1/apps/search/documents/clubs.py
@@ -0,0 +1,43 @@
+"""Club documents."""
+
+from django_elasticsearch_dsl import Document, fields
+from apps.sports.models import Club, Sport
+from django_elasticsearch_dsl.registries import registry
+
+
+@registry.register_document
+class ClubDocument(Document):
+    """Club elasticsearch document."""
+
+    sport = fields.ObjectField(properties={
+        'name': fields.TextField(),
+        'slug': fields.TextField(),
+        'icon': fields.FileField()
+    })
+
+    def get_queryset(self):
+        """Improve performance we can select related in one sql request."""
+        query = super(ClubDocument, self).get_queryset().select_related('profile')
+        return query
+
+    def get_instances_from_related(self, related_instance):
+        if isinstance(related_instance, Sport):
+            return related_instance.club_set.all()
+
+    class Index:
+        name = 'clubs'
+        settings = {
+            'number_of_shards': 1,
+            'number_of_replicas': 0
+        }
+
+    class Django:
+        model = Club
+        related_models = (Sport,)
+        fields = (
+            'name',
+            'slug',
+            'photo',
+            'cover_photo',
+            'city'
+        )
