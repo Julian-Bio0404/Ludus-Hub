63efa61b0b3014ca9c560c32e11ed6ecff7cf5e5
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2023-08-12 20:39:52 -0500

    [draws] Add automatic sport rules

--- web-1/taskapp/tasks/__init__.py
+++ web-1/taskapp/tasks/__init__.py
@@ -1,4 +1,5 @@
 from .payments import (create_stripe_plan, delete_stripe_plan,  # NOQA
                        update_stripe_plan)
+from .sports import create_sport_rules  # NOQA
 from .users import (send_restore_password_email, send_update_email,  # NOQA
                     send_verification_email)
